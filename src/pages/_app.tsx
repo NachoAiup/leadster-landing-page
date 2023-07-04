0a7541b7b58a8465e20011fd43e3594f21c86045
unknown <xnano_96@hotmail.com>
2023-07-04 12:22:10 -0300

    initial setup: prettier format and fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
+const myFont = localFont({
+  src: [
+    {
+      path: "./PlusJakartaSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./PlusJakartaSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={myFont.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
